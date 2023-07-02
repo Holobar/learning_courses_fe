ebb67cacda55464e91b109d60d4ab0b7ff73f90d
Aljaz Holobar <aljaz.holobar@gmail.com>
2023-07-02 23:26:36 +0200

    Working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from "./Pages/Home";
+import Wrapper from "./components/Wrapper";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import About from "./Pages/About";
+import CourseWeeks from "./Pages/CourseWeeks";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+          <Wrapper>
+              <BrowserRouter>
+                  <Routes>
+                      <Route path={'/'} element={<Home />} />
+                      <Route path={'/login'} element={<Login />} />
+                      <Route path={'/register'} element={<Register />} />
+                      <Route path={'/about'} element={<About />} />
+                      <Route path={'/courseWeeks/:courseId'} element={<CourseWeeks />} />
+                  </Routes>
+              </BrowserRouter>
+          </Wrapper>
+      </>);
 }
 
 export default App;
